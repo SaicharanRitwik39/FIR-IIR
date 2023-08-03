66b5e1ecfbc35b202a83b818abc72f2f0873a539
Saicharan Ritwik Chinni <109512004+SaicharanRitwik39@users.noreply.github.com>
2023-08-03 17:30:27 +0530

    Update IIR.v

--- IIR/Verilog/IIR.v
+++ IIR/Verilog/IIR.v
@@ -37,3 +37,22 @@ module full_adder(x,y,cin,s,cout);
   assign s = x^y;
   assign cout = x&y;
 endmodule
+
+module IIRfilter(clk,rst,a,x,y);
+  input clk,rst;
+  input [3:0] a,x;
+  output [3:0] y;
+  reg [3:0] y_val;
+  wire [7:0] baugh_prod_actual;
+  baugh_mult bm1(.a(a),.b(y_val),.p(baugh_prod_actual));
+  always@(posedge clk,rst,x,a)
+    begin
+      if(rst) begin
+        y_val <= x;
+      end
+      else begin
+        y_val <= x+booth_prod_actual[3:0];
+      end
+    end
+  assign y = y_val;
+endmodule
