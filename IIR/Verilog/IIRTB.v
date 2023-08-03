b4f689b43070ff1bb71c7c3f34a159be8953fb58
Saicharan Ritwik Chinni <109512004+SaicharanRitwik39@users.noreply.github.com>
2023-08-03 17:34:43 +0530

    Update IIRTB.v

--- IIR/Verilog/IIRTB.v
+++ IIR/Verilog/IIRTB.v
@@ -1,1 +1,19 @@
-
+module IIRfilter_tb;
+  reg clk,rst;
+  reg [3:0] a,x;
+  wire [3:0] y;
+  IIRfilter IIR1(.clk(clk),.rst(rst),.a(a),.x(x),.y(y));
+  initial begin
+    clk = 0;
+  end
+  always
+    #5 clk=~clk;
+  intitial begin
+    rst = 1;
+    a = 4'd2;
+    x = 4'd1;
+    #10 rst = 0;
+    a = 4'd2;
+    x = 4'd1;
+  end
+endmodule
